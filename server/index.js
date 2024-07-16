b8fdc72a1cfa35fea2761d32ecd476e291ecc778
MADHANRAJ-R <madhansr1509@gmail.com>
2024-07-16 23:55:20 +0530

    build9 commit

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3001;
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://samplevirtualdesign.vercel.app"],
     methods: ["POST", "GET", "PUT", "DELETE"],
     credentials: true,
   })
