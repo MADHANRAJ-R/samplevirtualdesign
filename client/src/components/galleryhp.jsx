b71e9a3b63666eabb4ab0587cd1572781bd87de5
MADHANRAJ-R <madhansr1509@gmail.com>
2024-08-27 13:34:00 +0530

    build11 commit

--- client/src/components/galleryhp.jsx
+++ client/src/components/galleryhp.jsx
@@ -9,13 +9,12 @@ const Galleryhp = () => {
           <p>[PHOTOGRAPHY]</p>
           <h1>Gallery</h1>
         </div>
-        <div className="squarediv">
+        {/* <div className="squarediv">
           <div className="square sqr0" Style="--i:0;"></div>
           <div className="square sqr1" Style="--i:1;"></div>
           <div className="square sqr2" Style="--i:2;"></div>
           <div className="square sqr3" Style="--i:3;"></div>
-          {/* <div className="square" Style="--i:4;"></div> */}
-        </div>
+        </div> */}
       </div>
     </section>
   );
